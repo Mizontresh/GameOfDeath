3e95d1016ae92b6f706f477f946346f04e93cb9a
Mizontresh <mizontresh@gmail.com>
2025-04-18 21:22:22 +0200

    taiko33

--- contracts/Mizons.sol
+++ contracts/Mizons.sol
@@ -4,19 +4,31 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title Mizons ERC‑20 with single and batch minting by owner
 contract Mizons is ERC20, Ownable {
-    constructor() ERC20("Mizons", "MIZ") {}
+    /// @param initialOwner the address that will be set as owner
+    constructor(address initialOwner)
+        ERC20("Mizons", "MIZ")
+        Ownable(initialOwner)
+    {
+        // no initial supply
+    }
 
     /// @notice Mint `amount` tokens to `to`
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 
-    /// @notice Batch‐mint tokens. `tos.length` must equal `amounts.length`.
-    function batchMint(address[] calldata tos, uint256[] calldata amounts) external onlyOwner {
-        require(tos.length == amounts.length, "Mizons: mismatched arrays");
-        for (uint256 i = 0; i < tos.length; i++) {
-            _mint(tos[i], amounts[i]);
+    /// @notice Mint to many addresses in one call
+    /// @param to list of recipient addresses
+    /// @param amounts list of amounts, must be same length as `to`
+    function mintBatch(address[] calldata to, uint256[] calldata amounts)
+        external
+        onlyOwner
+    {
+        require(to.length == amounts.length, "Mizons: length mismatch");
+        for (uint256 i = 0; i < to.length; i++) {
+            _mint(to[i], amounts[i]);
         }
     }
 }
