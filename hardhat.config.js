49354b683f50f8fbb0a5bfb493a26ef995761ff2
Mizontresh <mizontresh@gmail.com>
2025-05-28 08:10:03 +0200

    Taikooo

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+// hardhat.config.js
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    taiko: {
+      url: process.env.RPC_URL,          // e.g. https://rpc.ankr.com/taiko
+      chainId: 167000,
+      accounts: [ process.env.PRIVATE_KEY ]
+    },
+    // …other networks
+  },
 };
